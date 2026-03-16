131f82f61df1710d34a561a22d938948328b18d9
DublinNico <tonynico90@gmail.com>
2026-03-16 09:33:04 +0000

    Added Independent Class

--- DigiConnect/src/digiconnect/model/Independent.java
+++ DigiConnect/src/digiconnect/model/Independent.java
@@ -8,6 +8,32 @@ package digiconnect.model;
  *
  * @author tonyn
  */
-public class Independent {
+public class Independent extends Resident {
+    
+    private String completedProgramme;
+
+    public Independent(String completedProgramme, int id, String name, String address, int age, int phone, String digitalBarrier, String status) {
+        super(id, name, address, age, phone, digitalBarrier, status);
+        this.completedProgramme = completedProgramme;
+    }
+    
+    @Override
+    public String getSkillTier() {
+        return "Independent";
+    }
+
+    public String getCompletedProgramme() {
+        return completedProgramme;
+    }
+
+    public void setCompletedProgramme(String completedProgramme) {
+        this.completedProgramme = completedProgramme;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + " | Completed Programme " + completedProgramme;
+    }
+
     
 }
