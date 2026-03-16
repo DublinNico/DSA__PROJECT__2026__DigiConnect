19e25c193888d08c471683e956489b95456dc033
DublinNico <tonynico90@gmail.com>
2026-03-16 12:13:18 +0000

    Added Node class

--- DigiConnect/src/digiconnect/adt/Node.java
+++ DigiConnect/src/digiconnect/adt/Node.java
@@ -5,9 +5,41 @@
 package digiconnect.adt;
 
 /**
- *
+ * Represents a single node in a linked list
+ * Each node hols data and reference to the 
+ * next node in the chain
  * @author tonyn
  */
 public class Node {
     
+    // Data stored in the node
+    private Object element;
+    
+    // Reference to the next node in the list
+    private Node next;
+    
+    // Constructor that creates a node with element and references next node
+    public Node(Object e, Node n) {
+        element = e;
+        next = n;
+    }
+    // Returns the data stored in this node
+    public Object getElement() {
+        return element;
+    }
+    
+    // Sets the data stored in this node
+    public void setElement(Object element) {
+        this.element = element;
+    }
+
+    // Returns the next node in the linked list
+    public Node getNext() {
+        return next;
+    }
+
+    // Sets the next node in the linked list
+    public void setNext(Node next) {
+        this.next = next;
+    }   
 }
